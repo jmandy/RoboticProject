e72870779c95d771c5f79b57e05b95bd9a29f9f0
jmandy <jmandy@naver.com>
2018-11-24 18:53:25 +0900

    1
    1

---
+++ Robot/app/src/main/java/com/example/jmand/robot/FirstActivity.java
@@ -0,0 +1,31 @@
+package com.example.jmand.robot;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+        ImageButton setBtn=(ImageButton)findViewById(R.id.setting);
+
+
+        setBtn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(FirstActivity.this,MainActivity.class);
+                startActivity(intent);
+                 }
+        }) ;
+
+
+
+    }
+}
