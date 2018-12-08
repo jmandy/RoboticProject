6e168f20a8aadbf5b541782324572046391ac774
jmandy <jmandy@naver.com>
2018-12-08 13:56:03 +0900

    uupdate
    uupdate

--- Robot/app/src/main/java/com/example/jmand/robot/FirstActivity.java
+++ Robot/app/src/main/java/com/example/jmand/robot/FirstActivity.java
@@ -15,6 +15,8 @@ public class FirstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
         ImageButton setBtn=(ImageButton)findViewById(R.id.setting);
+        Button milkBtn=(Button)findViewById(R.id.btn1);
+        Button singBtn=(Button)findViewById(R.id.btn2);
 
 
         setBtn.setOnClickListener(new Button.OnClickListener() {
@@ -23,6 +25,22 @@ public class FirstActivity extends AppCompatActivity {
                 Intent intent=new Intent(FirstActivity.this,MainActivity.class);
                 startActivity(intent);
                  }
+        }) ;
+
+        milkBtn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(FirstActivity.this,MilkActivity.class);
+                startActivity(intent);
+            }
+        }) ;
+
+        singBtn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(FirstActivity.this,SongActivity.class);
+                startActivity(intent);
+            }
         }) ;
 
 
---
+++ Robot/app/src/main/java/com/example/jmand/robot/MilkActivity.java
@@ -0,0 +1,13 @@
+package com.example.jmand.robot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MilkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_milk);
+    }
+}
